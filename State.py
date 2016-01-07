64191c263e389dc13a73ade2c13515db99889155
cristinabucatea <cristina.bucatea@gmail.com>
2016-01-07 22:49:54 +0200

    added back some transition related code

--- State.py
+++ State.py
@@ -19,7 +19,8 @@ class State:
         self.arcs = collections.OrderedDict()
         self.selected = False
         self.state_name = state_name
-
+        self.up = True
+         
     def set_type(self, state_type):
         self.type = state_type
 
@@ -51,15 +52,26 @@ class State:
 
         for arc in self.arcs:
             if arc.contains_arc(self):
+                if arc == self:
                     dc.DrawText(self.state_name+'->'+arc.state_name+":"+self.arcs[arc], (self.position[0])-20, (self.position[1]- 100))
                     dc.DrawLine(self.position[0], self.position[1], arc.position[0], arc.position[1]-85)
+                    continue
+                else:
+                    if arc.up:
+                        self.up = False
+                    if self.up:
+                        dc.DrawText(self.state_name+'->'+arc.state_name+":"+self.arcs[arc], (self.position[0]+arc.position[0])/2,
+                                (self.position[1]+arc.position[1])/2 + 10)
+                    else:
+                        dc.DrawText(self.state_name+'->'+arc.state_name+":"+self.arcs[arc], (self.position[0]+arc.position[0])/2,
+                                (self.position[1]+arc.position[1])/2 - 10)
             else:
                 dc.DrawText(self.state_name+'->'+arc.state_name+":"+self.arcs[arc], (self.position[0]+arc.position[0])/2,
                             (self.position[1]+arc.position[1])/2 + 10)
-                dc.DrawLine(self.position[0], self.position[1], arc.position[0], arc.position[1])
+            dc.DrawLine(self.position[0], self.position[1], arc.position[0], arc.position[1])
 
         dc.DrawText(self.state_name, self.position[0], self.position[1])
-
+            
     def set_selected(self, selected):
         self.selected = selected
 
