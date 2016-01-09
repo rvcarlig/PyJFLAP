5dad86b4fe3e745adae077831de8c93fbef6d027
alexbucataru <bucatarualexandru93@gmail.com>
2016-01-09 04:38:49 +0200

    Step by step simulation

--- helpers.py
+++ helpers.py
@@ -113,6 +113,7 @@ class RunWind(wx.Frame):
         self.runlblname = wx.StaticText(self.panel, label="Input:")
         self.resultlbl = wx.StaticText(self.panel)
         self.close_button = wx.Button(self.panel, label="Close")
+        self.sim_button = wx.Button(self.panel, label="Step by step simulation")
         self.input = wx.TextCtrl(self.panel, size=(140, -1))
         
         self.windowSizer = wx.BoxSizer()
@@ -122,8 +123,9 @@ class RunWind(wx.Frame):
         self.sizer.Add(self.runlblname, (0, 0))
         self.sizer.Add(self.input, (0, 1))
         self.sizer.Add(self.resultlbl, (1, 0))
-        self.sizer.Add(self.run_button, (2, 0))        
-        self.sizer.Add(self.close_button, (2, 1))
+        self.sizer.Add(self.run_button, (2, 0))
+        self.sizer.Add(self.sim_button, (2, 1))
+        self.sizer.Add(self.close_button, (2, 2))
         
         self.border = wx.BoxSizer()
         self.border.Add(self.sizer, 1, wx.ALL | wx.EXPAND, 5)
@@ -134,6 +136,7 @@ class RunWind(wx.Frame):
         # Set event handlers
         self.close_button.Bind(wx.EVT_BUTTON, self.on_finish)
         self.run_button.Bind(wx.EVT_BUTTON, self.on_run)
+        self.sim_button.Bind(wx.EVT_BUTTON, self.on_simulation)
 
     def on_finish(self, event):
         self.controller.Enable()
@@ -142,3 +145,85 @@ class RunWind(wx.Frame):
     def on_run(self, event):
         self.resultlbl.SetLabel("Running...")
         self.resultlbl.SetLabel(self.controller.verifyInput(self.input.GetValue()))
+
+    def on_simulation(self, event):
+        self.controller.setup_sim()
+        sim_window = SimWind(self.controller)
+        sim_window.Show()        
+
+class SimWind(wx.Frame):
+    def __init__(self, controller, parent=None):
+        super(SimWind, self).__init__(parent, size=(200, 200))
+        self.controller = controller
+        self.controller.Disable()
+        self.panel = wx.Panel(self)
+        self.defaultColor = self.panel.GetBackgroundColour()
+        self.next_button = wx.Button(self.panel, label="Step")
+        self.close_button = wx.Button(self.panel, label="Close")
+        self.input = self.controller.run_win.input.GetValue()
+        self.input_values = []
+        self.input_sizer = wx.GridBagSizer()
+        self.button_sizer = wx.GridBagSizer()
+        self.string_index = 0
+        self.result = wx.StaticText(self.panel)        
+        if self.controller.doodle.start_state == None:
+            self.result.SetLabel("No Start State")
+        elif self.input == "":
+            self.result.SetLabel("No Input")
+            
+
+        self.font = wx.Font(18, wx.DECORATIVE, wx.NORMAL, wx.BOLD)
+
+        self.windowSizer = wx.BoxSizer()
+        self.windowSizer.Add(self.panel, 1, wx.ALL | wx.EXPAND)
+        
+        self.sizer = wx.GridBagSizer(5, 5)
+
+        i = 0
+        for c in self.input:            
+            new_text_box = wx.StaticText(self.panel)
+            new_text_box.SetLabel(c)
+            new_text_box.SetFont(self.font)
+            new_text_box.SetForegroundColour('Black')
+            self.input_values.append(new_text_box)
+            self.input_sizer.Add(new_text_box, (0, i))            
+            i += 1
+        
+        self.sizer.Add(self.input_sizer, (0, 0))
+        self.button_sizer.Add(self.next_button, (0, 0))
+        self.button_sizer.Add(self.close_button, (0, 1))
+        self.sizer.Add(self.result, (1, 0))
+        self.sizer.Add(self.button_sizer, (2, 0))
+
+        self.border = wx.BoxSizer()
+        self.border.Add(self.sizer, 1, wx.ALL | wx.EXPAND, 5)
+
+        self.panel.SetSizerAndFit(self.border)
+        self.SetSizerAndFit(self.windowSizer)
+
+        self.next_button.Bind(wx.EVT_BUTTON, self.on_next)
+        self.close_button.Bind(wx.EVT_BUTTON, self.on_finish)
+
+    def on_finish(self, event):
+        for state in self.controller.doodle.states:
+            state.current = False
+            state.bad_input = False
+            state.ok_input = False
+        self.controller.doodle.redraw()
+        self.controller.Enable()
+        self.Close()
+
+    def on_next(self, event):
+        if self.controller.doodle.start_state != None and self.controller.finish == False:
+            if self.string_index > 0:
+                self.input_values[self.string_index - 1].SetLabel(self.input_values[self.string_index - 1].GetLabel())
+                self.input_values[self.string_index - 1].SetBackgroundColour(self.defaultColor)
+                self.input_values[self.string_index - 1].SetForegroundColour('Black')
+            if self.string_index < len(self.input_values):
+                self.input_values[self.string_index].SetLabel(self.input_values[self.string_index].GetLabel())
+                self.input_values[self.string_index].SetBackgroundColour('White')
+                self.input_values[self.string_index].SetForegroundColour('Red')
+                self.result.SetLabel(self.controller.sim_step(self.input[self.string_index],
+                                         True if self.string_index == len(self.input) - 1 else False))
+                self.controller.doodle.redraw()
+                self.string_index += 1
